3b8fff3c48eb2c7210014cf90867eae1aca05692
Your Name <you@example.com>
2018-07-20 08:32:56 +0800

    change luci cbi

--- files/autossh_cbi.lua
+++ files/autossh_cbi.lua
@@ -21,6 +21,9 @@ o.placeholder = 22
 o.datatype    = "port"
 o.rmempty     = false
 
+o = s:option(Value, "localaddr", translate("本地地址"))
+o.rmempty     = false
+
 o = s:option(Value, "remoteport", translate("远程端口"))
 o.placeholder = 19999
 o.datatype    = "port"
@@ -33,35 +36,18 @@ o.rmempty     = false
 o = s:option(Value, "ssh", translate("远程登陆"))
 o.rmempty     = false
 
-o = s:option(Value, "AUTOSSH_FIRST_POLL", translate("AUTOSSH_FIRST_POLL"))
-o.datatype    = "uinteger"
 
-o = s:option(Value, "AUTOSSH_GATETIME", translate("AUTOSSH_GATETIME"))
-o.placeholder = 30
-o.datatype    = "uinteger"
-
-o = s:option(Value, "AUTOSSH_LOGLEVEL", translate("AUTOSSH_LOGLEVEL"))
-o.datatype    = "and(uinteger,max(7))"
-
-o = s:option(Value, "AUTOSSH_LOGFILE", translate("AUTOSSH_LOGFILE"))
-
-o = s:option(Value, "AUTOSSH_MAXLIFETIME", translate("AUTOSSH_MAXLIFETIME"))
-
-o = s:option(Value, "AUTOSSH_MAXSTART", translate("AUTOSSH_MAXSTART"))
-o.placeholder = -1
-o.datatype    = "integer"
-
-o = s:option(Value, "AUTOSSH_MESSAGE", translate("AUTOSSH_MESSAGE"))
-
-o = s:option(Value, "AUTOSSH_PATH", translate("AUTOSSH_PATH"))
-
-o = s:option(Value, "AUTOSSH_PIDFILE", translate("AUTOSSH_PIDFILE"))
-
-o = s:option(Value, "AUTOSSH_POLL", translate("AUTOSSH_POLL"))
-o.placeholder = 600
-o.datatype    = "uinteger"
-
-o = s:option(Value, "AUTOSSH_PORT", translate("AUTOSSH_PORT"))
+o = s:option(Value, "remoteport_maintain", translate("维护远程端口"))
+o.placeholder = 19999
 o.datatype    = "port"
+o.rmempty     = false
+
+o = s:option(Value, "monitorport_maintain", translate("维护监视端口"))
+o.datatype    = "port"
+o.rmempty     = false
+
+o = s:option(Value, "ssh_maintain", translate("维护远程登陆"))
+o.rmempty     = false
+
 
 return m
